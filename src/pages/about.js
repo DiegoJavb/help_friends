73dfba4d881ab25c6d99bff9a2c7af319a7378da
DESKTOP-AD5PBAC\diego <diegohd_20@outlook.com>
2021-02-28 02:12:33 -0500

    Cambio en la estructura de directorios

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+const About = () => {
+    return (
+        <Button variant="contained" color="primary">
+        Hola Mundo!
+        </Button>
+    );
+};
+
+export default About;
